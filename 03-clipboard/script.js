ac60d77f9319a0be2b0d5eb63478ad6681a5133c
abneribeiroo <eliasabner38@gmail.com>
2025-02-23 21:36:59 +0000

    feat: improve script

--- 03-clipboard/script.js
+++ 03-clipboard/script.js
@@ -2,6 +2,8 @@ const txt = document.querySelector("#txt");
 const button = document.querySelector("#copy");
 
 button.addEventListener("click", () => {
-  navigator.clipboard.writeText(txt.value);
-  navigator.clipboard.readText().then((clip) => alert(clip));
+  // navigator.clipboard.writeText(txt.value);
+  // navigator.clipboard.readText().then((clip) => alert(clip));^
+  
 });
+
