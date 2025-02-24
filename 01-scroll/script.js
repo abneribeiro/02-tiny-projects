60eb745be158e8b3093678fed582e045364a0ad5
abneribeiroo <eliasabner38@gmail.com>
2025-02-24 22:09:27 +0000

    feat: improved code for image

--- 01-scroll/script.js
+++ 01-scroll/script.js
@@ -1,3 +1,8 @@
+"strict"
+
+import data from "./data.json"  with { type: "json" };
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const carousel = document.querySelector(".carousel")
   const slides = document.querySelectorAll(".slide")
@@ -5,12 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let currentSlide = 0
 
+  data.images.forEach((img, index) =>{
+    carousel.insertAdjacentHTML("beforeend", `<div class="slide"><img src="${img}" alt=""></div>`);
+    
+  })
+
   const updateIndicators = () => {
     indicators.forEach((indicator, index) => {
       indicator.classList.toggle("active", index === currentSlide)      
-      let url = (slides[index].children[0].getAttribute("src"));
       
-      indicator.innerHTML= `<img src=${url} alt="">`
+      indicator.innerHTML= `<img src=${data.images[index]} alt="">`
     })
   }
 
